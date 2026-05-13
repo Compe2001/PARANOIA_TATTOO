e8b77d3b5a237300704795e786600c17837d96da
Compe2001 <casielrodriguez14@gmail.com>
2026-05-13 12:01:03 -0600

    ssus

--- preloader.js
+++ preloader.js
@@ -1,10 +1,5 @@
-window.addEventListener("load", () => {
-  const preloader = document.getElementById("preloader");
-  const content = document.getElementById("content");
-
-  preloader.style.opacity = "0";
+ preloader.style.opacity = "0";
   setTimeout(() => {
     preloader.style.display = "none";
     content.style.display = "block";
   }, 1000); // tiempo del fade-out
-});
